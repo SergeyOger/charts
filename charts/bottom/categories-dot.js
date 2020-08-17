4005bebc797a6af303bfdb79a9e7f95952f45c6e
Sergey Oger <sergey.oger@broscorp.net>
2020-08-17 10:59:57 +0300

    wip

---
+++ charts/bottom/categories-dot.js
@@ -0,0 +1,17 @@
+var ctx = document.getElementById('categories-dot').getContext('2d');
+
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data = {
+        datasets: [{
+            data: [10, 20, 30]
+        }],
+    
+        // These labels appear in the legend and in the tooltips when hovering different arcs
+        labels: [
+            'Red',
+            'Yellow',
+            'Blue'
+        ]
+    }
+});
--- charts/readiness.js
+++ charts/readiness.js
@@ -5,15 +5,47 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['January', 'February', 'March'],
         datasets: [{
-            label: 'Readiness',
-            backgroundColor: 'rgb(255, 99, 132)',
+            label: 'Readiness, % \n\r' +  getLableValue(),
+            fill: false,
+            backgroundColor: 'rgb(83, 85, 84)',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [0, 10, 5]
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+        legend: {
+            display: true,
+            align: 'center',
+            labels: {
+                fontColor: 'rgb(246, 244, 246)',
+                boxWidth: 0,
+                fontSize: 20
+            }
+        },
+
+        layout: {
+            padding: {
+                left: 10,
+                right: 10,
+                top: 10,
+                bottom: 10
+            }
+        },
+        scales: {
+            xAxes: [{
+                display: false
+            }],
+            yAxes: [{
+                display: false
+            }]
+        }
+    }
 });
+
+function  getLableValue() {
+    return 50
+};
